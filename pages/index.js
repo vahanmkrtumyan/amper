6893eb18798272ad6aa65793ccf497da121ac565
vahanmkrtumyanqless <97587857+vahanmkrtumyanqless@users.noreply.github.com>
2023-05-24 12:28:49 +0400

    Header style changes

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,19 @@
-import Image from 'next/image'
-import Layout from '../src/layout/layout'
-import HeroHeader from '../src/components/hero_header'
-import HomeIntroduce from '../src/components/home_introduce'
-import Siderbar from '../src/components/sidebar'
-import HomeService from '../src/components/home_service'
-import WhyChooseUs from '../src/components/why_choose_us'
-import Principles from '../src/components/principles'
-import Responsibility from '../src/components/responsibility'
-import HomeProject from '../src/components/home_project'
-import HomeBlog from '../src/components/home_blog'
+import Image from "next/image";
+import Layout from "../src/layout/layout";
+import HeroHeader from "../src/components/hero_header";
+import HomeIntroduce from "../src/components/home_introduce";
+import Siderbar from "../src/components/sidebar";
+import HomeService from "../src/components/home_service";
+import WhyChooseUs from "../src/components/why_choose_us";
+import Principles from "../src/components/principles";
+import Responsibility from "../src/components/responsibility";
+import HomeProject from "../src/components/home_project";
+import HomeBlog from "../src/components/home_blog";
 
 export default function Home() {
   return (
     <>
-      
-
-      <Layout style={"transparrent"}>
+      <Layout style={"light"}>
         {/* HERO HEADER */}
         <HeroHeader />
         {/* /HERO HEADER */}
@@ -41,8 +39,6 @@ export default function Home() {
         <HomeBlog />
         {/* /HOME BLOG */}
       </Layout>
-
-
     </>
-  )
+  );
 }
